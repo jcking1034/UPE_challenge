7459251e79e30c9d50e31bd204d6347fdc12eb57
James King <jcking1034@gmail.com>
2019-05-19 23:40:11 -0700

    Update minimal_maze_solver.py

--- minimal_maze_solver.py
+++ minimal_maze_solver.py
@@ -11,13 +11,10 @@ for i in range(5):
         next_spot = maze_stack.pop()
         while abs(next_spot[0] - new_body['cur_loc'][0]) + abs(next_spot[1] - new_body['cur_loc'][1]) > 1:
             last_move = move_list.pop()
-            action = move_dict[last_move][2]
-            new_body['cur_loc'][move_dict[last_move][0]] -= move_dict[last_move][1]
-            body = requests.post('http://ec2-34-211-81-131.us-west-2.compute.amazonaws.com' + '/game?token=' + access_token, data = {'action': action}).json()
+            new_body['cur_loc'][move_dict[last_move][0]], body = new_body['cur_loc'][move_dict[last_move][0]] - move_dict[last_move][1], requests.post('http://ec2-34-211-81-131.us-west-2.compute.amazonaws.com' + '/game?token=' + access_token, data = {'action': move_dict[last_move][2]}).json()
         if next_spot[0] != new_body['cur_loc'][0]:  action = 'right' if next_spot[0] > new_body['cur_loc'][0] else 'left'
-        else:                           action = 'down' if next_spot[1] > new_body['cur_loc'][1] else 'up'
-        body = requests.post('http://ec2-34-211-81-131.us-west-2.compute.amazonaws.com' + '/game?token=' + access_token, data = {'action': action}).json()
-        maze[next_spot[0]][next_spot[1]] = 1
+        else:                                       action = 'down' if next_spot[1] > new_body['cur_loc'][1] else 'up'
+        body, maze[next_spot[0]][next_spot[1]] = requests.post('http://ec2-34-211-81-131.us-west-2.compute.amazonaws.com' + '/game?token=' + access_token, data = {'action': action}).json(), 1
         if body['result'] == 0:     
             new_body['cur_loc'][move_dict[action][0]] += move_dict[action][1]
             move_list.append(action)
