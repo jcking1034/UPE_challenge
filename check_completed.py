56224e10348badb5372a9858e556f128b22d4b16
James King <jcking1034@gmail.com>
2019-05-19 19:50:08 -0700

    updates2

--- check_completed.py
+++ check_completed.py
@@ -3,4 +3,4 @@ import requests
 # Get Access Token
 url, id = 'http://ec2-34-211-81-131.us-west-2.compute.amazonaws.com', '205125796'
 access_token = requests.post(url + '/session', data = {'uid': id}).json()['token']
-print(f"access token: {access_token}\nGame Status: {requests.get(url + '/game?token=' + access_token).json()['status']}")
+print(f"access token: {access_token}\nGame Status: {requests.get(url + '/game?token=' + access_token).json()}")
--- minimal_maze_solver.py
+++ minimal_maze_solver.py
@@ -1,15 +1,13 @@
 import requests
-access_token = requests.post('http://ec2-34-211-81-131.us-west-2.compute.amazonaws.com' + '/session', data = {'uid': '205125796'}).json()['token']
-move_dict = {'up': (1, -1, 'down'), 'down': (1, +1, 'up'), 'right': (0, +1, 'left'), 'left': (0, -1, 'right')}
+access_token, move_dict = requests.post('http://ec2-34-211-81-131.us-west-2.compute.amazonaws.com' + '/session', data = {'uid': '205125796'}).json()['token'], {'up': (1, -1, 'down'), 'down': (1, +1, 'up'), 'right': (0, +1, 'left'), 'left': (0, -1, 'right')}
 for i in range(5):
-    new_body = requests.get('http://ec2-34-211-81-131.us-west-2.compute.amazonaws.com' + '/game?token=' + access_token).json()    # Get new game
-    maze_size = new_body['size'][0], new_body['size'][1]
+    new_body = requests.get('http://ec2-34-211-81-131.us-west-2.compute.amazonaws.com' + '/game?token=' + access_token).json()
     cur_loc = [new_body['cur_loc'][0], new_body['cur_loc'][1]]
-    maze, move_list, maze_stack = [[1 if i == cur_loc[1] and j == cur_loc[0] else 0 for i in range(maze_size[1])] for j in range(maze_size[0])], [], []
+    maze, move_list, maze_stack = [[1 if i == cur_loc[1] and j == cur_loc[0] else 0 for i in range(new_body['size'][1])] for j in range(new_body['size'][0])], [], []
     while True:
-        if cur_loc[0] + 1 < maze_size[0] and maze[cur_loc[0] + 1][cur_loc[1]] == 0 and (cur_loc[0] + 1, cur_loc[1]) not in maze_stack:  maze_stack.append((cur_loc[0] + 1, cur_loc[1]))
+        if cur_loc[0] + 1 < new_body['size'][0] and maze[cur_loc[0] + 1][cur_loc[1]] == 0 and (cur_loc[0] + 1, cur_loc[1]) not in maze_stack:  maze_stack.append((cur_loc[0] + 1, cur_loc[1]))
         if cur_loc[0] - 1 >= 0 and maze[cur_loc[0] - 1][cur_loc[1]] == 0 and (cur_loc[0] - 1, cur_loc[1]) not in maze_stack:            maze_stack.append((cur_loc[0] - 1, cur_loc[1]))
-        if cur_loc[1] + 1 < maze_size[1] and maze[cur_loc[0]][cur_loc[1] + 1] == 0 and (cur_loc[0], cur_loc[1] + 1) not in maze_stack:  maze_stack.append((cur_loc[0], cur_loc[1] + 1))
+        if cur_loc[1] + 1 < new_body['size'][1] and maze[cur_loc[0]][cur_loc[1] + 1] == 0 and (cur_loc[0], cur_loc[1] + 1) not in maze_stack:  maze_stack.append((cur_loc[0], cur_loc[1] + 1))
         if cur_loc[1] - 1 >= 0 and maze[cur_loc[0]][cur_loc[1] - 1] == 0 and (cur_loc[0], cur_loc[1] - 1) not in maze_stack:            maze_stack.append((cur_loc[0], cur_loc[1] - 1))
         next_spot = maze_stack.pop()
         while abs(next_spot[0] - cur_loc[0]) + abs(next_spot[1] - cur_loc[1]) > 1:
